3de2289f6a865c514bcce2118b5de5cc839692e6
YanaMr <UDvtneGP224ZMLz>
2020-11-23 21:36:48 +0100

    test created

--- src/test/java/apiTests/EmailValidationTest.java
+++ src/test/java/apiTests/EmailValidationTest.java
@@ -2,10 +2,19 @@ package apiTests;
 
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
+import pojo.Comment;
+import pojo.Post;
+import pojo.User;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Pattern;
 
 import static io.restassured.RestAssured.given;
 import static java.net.HttpURLConnection.HTTP_OK;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class EmailValidationTest {
 
@@ -13,6 +22,7 @@ public class EmailValidationTest {
     private static final String POSTS_SEARCH_URL = "https://jsonplaceholder.typicode.com/posts";
     private static final String COMMENTS_SEARCH_URL = "https://jsonplaceholder.typicode.com/comments";
     private static final String USERNAME = "Delphine";
+    private static final Pattern EMAIL_REGEX = Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
 
     @Test
     public void Test() {
@@ -20,6 +30,32 @@ public class EmailValidationTest {
         Response personByUsername = getPersonByUsername(USERNAME);
         assertEquals(personByUsername.getStatusCode(), HTTP_OK);
 
+        User[] foundUsers = personByUsername.getBody().as(User[].class);
+        assertEquals(foundUsers.length, 1);
+        int userId = foundUsers[0].getId();
+
+        Response searchPosts = getUserPosts(userId);
+        assertEquals(personByUsername.getStatusCode(), HTTP_OK);
+
+        Post[] foundPosts = searchPosts.getBody().as(Post[].class);
+        ArrayList<Integer> postIds = new ArrayList<>();
+        for (int i = 0; i < foundPosts.length; i++) {
+            postIds.add(foundPosts[i].getId());
+        }
+
+        List<Comment> foundComments = new ArrayList<>();
+        for (int postId : postIds) {
+            foundComments.addAll(Arrays.asList(getPostComments(postId).getBody().as(Comment[].class)));
+        }
+
+        ArrayList<String> emails = new ArrayList<>();
+        for (int i = 0; i < foundComments.size(); i++) {
+            emails.add(foundComments.get(i).getEmail());
+        }
+
+        for (String value : emails) {
+            assertTrue(EMAIL_REGEX.matcher(value).matches());
+        }
 
     }
 
